c539b8ca9b8b05cabee00bd6de4defb35ef457d8
Jinjing Wang <nfjinjing@gmail.com>
2015-07-23 15:51:59 +0800

    use safeSocket

--- src/Main.hs
+++ src/Main.hs
@@ -10,33 +10,34 @@ import System.Posix.Signals
 import Control.Exception
 import System.IO
 
+pute :: String -> IO ()
+pute = hPutStrLn stderr
+
+safeSocket :: String -> Socket -> (Socket -> IO a) -> IO a
+safeSocket aID aSocket f =
+  catch (f aSocket) - \e -> do
+      pute - "Exception in " + aID + ": " + show (e :: SomeException)
+      sClose aSocket
+      throw e
 
 main :: IO ()
 main = do
   puts "Started!"
-  _socket <- socket AF_INET Stream defaultProtocol
+  _mainSocket <- socket AF_INET Stream defaultProtocol
+  setSocketOption _mainSocket ReuseAddr 1
+  bindSocket _mainSocket (SockAddrInet 1091 iNADDR_ANY)
+  listen _mainSocket 1
 
-  setSocketOption _socket ReuseAddr 1
-  
-  bindSocket _socket (SockAddrInet 1091 iNADDR_ANY)
 
-  listen _socket 1
+  let handler aSocket = safeSocket "Connection Socket" aSocket - \_socket ->
+                  accept _socket >>= fork . socketHandler
 
-  let handler = 
-        catch (accept _socket >>= fork . socketHandler)- \e -> do
-            hPutStrLn stderr - "Handler Exception: " + show (e :: SomeException)
-            sClose _socket
-            throw e
-          
-
-  catch (forever (handler >> sleep 1)) - \e -> do
-    hPutStrLn stderr - "Exception: " + show (e :: SomeException)
-    sClose _socket
-    throw e
+  safeSocket "Main Socket" _mainSocket - forever . handler 
     
 
 socketHandler:: (Socket, SockAddr) -> IO ()
-socketHandler (_socket, _) = do
-  send _socket "Accepting: \n"
+socketHandler (_socket, _sockAddr) = do
+  puts - "Connected: " + show _sockAddr
+  send _socket - "Hi: " + show _sockAddr + "!\n"
   sClose _socket
 
