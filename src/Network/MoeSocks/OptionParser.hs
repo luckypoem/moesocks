4437c949d05db9c14f661d55a8e796deeb3f8bb3
Jinjing Wang <nfjinjing@gmail.com>
2015-07-25 19:11:46 +0800

    add option parser

---
+++ src/Network/MoeSocks/OptionParser.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE PackageImports #-}
+
+module Network.MoeSocks.OptionParser where
+
+import Control.Lens
+import Prelude ((.))
+import Air.Env hiding ((.), has, take, puts) 
+
+import Network.MoeSocks.Type
+import Network.MoeSocks.App
+
+import Data.ByteString.Lens
+import System.Random
+import qualified Prelude as P
+import "cipher-aes" Crypto.Cipher.AES
+import Data.Maybe
+import Data.Text.Lens
+
+import Options.Applicative hiding (Parser)
+import qualified Options.Applicative as O
+
+optionParser :: O.Parser MoeOptions
+optionParser = 
+  let _mode = strOption -
+                    short 'm'
+                 <> metavar "MODE"
+                 <> help "local | remote"
+  in
+  let _config = strOption -
+                    short 'c'
+                 <> metavar "CONFIG"
+                 <> help "path to the configuration file"
+
+  in
+
+  let parseMode :: String -> RunningMode
+      parseMode x 
+        | x `elem` ["server", "remote"] = ServerMode
+        | x `elem` ["client", "local"] = ClientMode
+        | x == "debug" = DebugMode
+  in
+
+  MoeOptions <$> fmap parseMode _mode <*> fmap (view packed) _config
+
+opts :: ParserInfo MoeOptions
+opts = info (helper <*> optionParser) - 
+        fullDesc
+    <>  progDesc "A socks5 proxy with client / server architecture"
+    <>  header "moesocks - moe for all"
+
+
