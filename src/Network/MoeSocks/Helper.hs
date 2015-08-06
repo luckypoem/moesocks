30a07749027d53f0f3f7c48b8b3f6a8a6f180cb5
Jinjing Wang <nfjinjing@gmail.com>
2015-08-06 21:27:39 +0800

    log exception

--- src/Network/MoeSocks/Helper.hs
+++ src/Network/MoeSocks/Helper.hs
@@ -122,6 +122,12 @@ catchIO aID aIO = catch (() <$ aIO) - \e ->
                 pute - "IOError in " <> aID <> ": " 
                   <> show (e :: IOException)
                 
+logException :: String -> IO a -> IO ()
+logException aID aIO = catch (() <$ aIO) - \e -> 
+                        do
+                          pute - "IOError in " <> aID <> ": " 
+                            <> show (e :: SomeException)
+                          throw e
 
 wrapIO :: (Maybe String, IO c) -> IO ()
 wrapIO (s,  _io) = do
@@ -129,7 +135,8 @@ wrapIO (s,  _io) = do
   {-forM_ s - puts . ("+ " <>)-}
   {-catchExceptAsyncLog (fromMaybe "" s) _io -}
   {-catchIO (fromMaybe "" s) _io -}
-  () <$ _io 
+  {-() <$ _io -}
+  logException (fromMaybe "" s) _io 
   {-catch  (() <$ _io) - \(e :: IOException) -> pure ()-}
     {-<* (forM_ s - puts . ("- " <>))-}
                 
