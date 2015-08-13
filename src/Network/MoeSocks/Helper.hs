f0c9f2f21e9555a440d798cbdedd84ec992e3bed
Jinjing Wang <nfjinjing@gmail.com>
2015-08-13 13:22:42 +0800

    perfer ipv4

--- src/Network/MoeSocks/Helper.hs
+++ src/Network/MoeSocks/Helper.hs
@@ -215,7 +215,7 @@ getSocket aHost aPort aSocketType = do
     hints = defaultHints {
               addrFlags = [AI_ADDRCONFIG, AI_NUMERICSERV]
             , addrSocketType = aSocketType
-            {-, addrFamily = AF_INET-}
+            , addrFamily = AF_INET
             }
 
 builder_To_ByteString :: B.Builder -> ByteString
