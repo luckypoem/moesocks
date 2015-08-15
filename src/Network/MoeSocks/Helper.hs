e7b5e3d3df6b0e9bc702d167160bb1fe46d53643
Jinjing Wang <nfjinjing@gmail.com>
2015-08-15 21:32:31 +0800

    test connectTunnel

--- src/Network/MoeSocks/Helper.hs
+++ src/Network/MoeSocks/Helper.hs
@@ -171,13 +171,13 @@ waitBothDebug x y = do
   puts - "All done for " <> _hID
   pure ()
 
-connectTunnel :: (Maybe String, IO ()) -> (Maybe String, IO ()) -> IO ()
-connectTunnel = waitBothDebug
+connectTunnel' :: (Maybe String, IO ()) -> (Maybe String, IO ()) -> IO ()
+connectTunnel' = waitBothDebug
 
-{-connectTunnel :: (Maybe String, IO ()) -> (Maybe String, IO ()) -> IO ()-}
-{-connectTunnel x y = -}
-  {-withAsync (logWaitIO x) - const - do-}
-    {-logWaitIO y-}
+connectTunnel :: (Maybe String, IO ()) -> (Maybe String, IO ()) -> IO ()
+connectTunnel x y = 
+  withAsync (logWaitIO x) - const - do
+    logWaitIO y
 
 connectMarket :: (Maybe String, IO ()) -> (Maybe String, IO ()) -> IO ()
 connectMarket = waitBothDebug
