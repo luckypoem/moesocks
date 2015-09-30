3fd80b0ce4033f8fe2e9e163b6ef47ff129eeecf
Jinjing Wang <nfjinjing@gmail.com>
2015-09-30 22:28:18 +0800

    add nix shell

---
+++ shell.nix
@@ -0,0 +1,38 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, async, attoparsec, base, binary
+      , bytestring, containers, cryptohash, hslogger, HsOpenSSL, iproute
+      , lens, lens-aeson, mtl, network, optparse-applicative, random
+      , stdenv, stm, strict, text, time, transformers
+      , unordered-containers
+      }:
+      mkDerivation {
+        pname = "moesocks";
+        version = "1.0.0.30";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          aeson async attoparsec base binary bytestring containers cryptohash
+          hslogger HsOpenSSL iproute lens lens-aeson mtl network
+          optparse-applicative random stm strict text time transformers
+          unordered-containers
+        ];
+        homepage = "https://github.com/nfjinjing/moesocks";
+        description = "A functional firewall killer";
+        license = stdenv.lib.licenses.asl20;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
