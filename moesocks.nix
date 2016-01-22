eaec385f1bcf0e206c1615fd2ad90cc1c25261b8
Jinjing Wang <nfjinjing@gmail.com>
2016-01-22 14:36:23 +0800

    add more options

--- moesocks.nix
+++ moesocks.nix
@@ -2,27 +2,96 @@
 
 with lib;
 
-let cfg = config.services.moesocks; in
+let
+  cfg = config.services.moesocks
+; confFile = pkgs.writeText "moesocks.json" (builtins.toJSON cfg)
+; localIPs =
+    [
+      "0.0.0.0/8"
+      "10.0.0.0/8"
+      "100.64.0.0/10"
+      "127.0.0.0/8"
+      "169.254.0.0/16"
+      "172.16.0.0/12"
+      "192.0.0.0/24"
+      "192.0.2.0/24"
+      "192.88.99.0/24"
+      "192.168.0.0/16"
+      "198.18.0.0/15"
+      "198.51.100.0/24"
+      "203.0.113.0/24"
+      "224.0.0.0/4"
+      "240.0.0.0/4"
+      "255.255.255.255/32"
+      "::/128"
+      "::1/128"
+      "::ffff:0:0/96,"
+      "100::/64"
+      "64:ff9b::/96"
+      "2001::/32"
+      "2001:10::/28"
+      "2001:20::/28"
+      "2001:db8::/32"
+      "2002::/16"
+      "fc00::/7"
+      "fe80::/10"
+      "ff00::/8"
+    ]
+; in
 
 { options =
     { services.moesocks =
-       { enable = mkOption
-           { type = types.bool
-           ; default = false
-           ; description = "Whether to run the moesocks socks5 proxy"
-           ; }
+        { enable = mkOption
+            { type = types.bool
+            ; default = false
+            ; description = "Whether to run the moesocks socks5 proxy"
+            ; }
 
-       ; tcp = mkOption
-           { type = types.listOf types.str
-           ; default = []
-           ; example = [ "5300:8.8.8.8:53" ]
-           ; description =
-               ''
-                 Specify that the given TCP port on the local(client)
-                 host is to be forwarded to the given host and port on
-                 the remote side.
-               ''
-           ; }
+        ; role = mkOption
+            { type = types.str
+            ; default = "local"
+            ; description = "Tell moesocks to run as local or remote"
+            ; }
+
+        ; tcp = mkOption
+            { type = types.listOf types.str
+            ; default = []
+            ; example = [ "5300:8.8.8.8:53" ]
+            ; description =
+                ''
+                  Specify that the given TCP port on the local(client)
+                  host is to be forwarded to the given host and port on
+                  the remote side.
+                ''
+            ; }
+
+        ; udp = mkOption
+            { type = types.listOf types.str
+            ; default = []
+            ; example = [ "5300:8.8.8.8:53" ]
+            ; description =
+                ''
+                  Specify that the given UDP port on the local(client)
+                  host is to be forwarded to the given host and port on
+                  the remote side.
+                ''
+            ; }
+
+        ; disableSOCKS5 = mkOption
+            { type = types.bool
+            ; default = false
+            ; description =
+                ''
+                  Do not start a SOCKS5 server on local. It can be
+                  useful to run moesocks only as a secure tunnel
+                ''
+            ; }
+
+        ; forbiddenIP = mkOption
+            { type = types.listOf types.str
+            ; default = localIPs
+            ; description = "IP list declared invalid as destinations"
+            ; }
 
         ; remote = mkOption
             { type = types.str
@@ -79,21 +148,21 @@ let cfg = config.services.moesocks; in
 
 ; config = mkIf cfg.enable
     { users.extraUsers = singleton
-      { name = "moesocks"
-      ; uid = 2000 # config.ids.uids.moesocks
-      ; description = "moesocks dummy user"
-      ; }
+        { name = "moesocks"
+        ; uid = 2000 # config.ids.uids.moesocks
+        ; description = "moesocks dummy user"
+        ; }
 
     ; systemd.services.moesocks =
-      { wantedBy = [ "multi-user.target" ]
-      ; after = [ "network.target" ]
-      ; description = "moesocks socks5 local proxy"
-      ; serviceConfig =
-          { User = "moesocks"
-          ; ExecStart =
-          ''
-             ${pkgs.haskellPackages.moesocks}/bin/moesocks -s ${cfg.remote} -p ${toString cfg.remotePort} -b ${cfg.local} -l ${toString cfg.localPort} -t ${toString cfg.timeout} -k ${cfg.password} -T ${concatStringsSep " " cfg.tcp} -m ${cfg.method} ${optionalString (cfg.fastOpen) "--fast-open"}
-          ''
+        { wantedBy = [ "multi-user.target" ]
+        ; after = [ "network.target" ]
+        ; description = "moesocks socks5 local proxy"
+        ; serviceConfig =
+            { User = "moesocks"
+            ; ExecStart =
+            ''
+             ${pkgs.haskellPackages.moesocks}/bin/moesocks -v -r ${cfg.role} ${optionalString (cfg.tcp != []) "-T ${concatStringsSep " " cfg.tcp}"} ${optionalString (cfg.udp != []) "-U ${concatStringsSep " " cfg.udp}"} ${optionalString (cfg.disableSOCKS5) "--disable-socks5"} -c ${confFile}
+            ''
           ; }
       ; }
     ; }
