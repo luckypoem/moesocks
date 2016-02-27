c9bf884ec990c4a6c2dc845b2af5a04af9ace2ad
Jinjing Wang <nfjinjing@gmail.com>
2016-02-27 12:33:13 +0800

    cleaner config file

--- nixos/proxy/moesocks.nix
+++ nixos/proxy/moesocks.nix
@@ -5,38 +5,38 @@ with lib;
 let
   cfg = config.services.moesocks;
   configFile = pkgs.writeText "moesocks.json" (builtins.toJSON cfg);
-  localIPs =
-    [
-      "0.0.0.0/8"
-      "10.0.0.0/8"
-      "100.64.0.0/10"
-      "127.0.0.0/8"
-      "169.254.0.0/16"
-      "172.16.0.0/12"
-      "192.0.0.0/24"
-      "192.0.2.0/24"
-      "192.88.99.0/24"
-      "192.168.0.0/16"
-      "198.18.0.0/15"
-      "198.51.100.0/24"
-      "203.0.113.0/24"
-      "224.0.0.0/4"
-      "240.0.0.0/4"
-      "255.255.255.255/32"
-      "::/128"
-      "::1/128"
-      "::ffff:0:0/96,"
-      "100::/64"
-      "64:ff9b::/96"
-      "2001::/32"
-      "2001:10::/28"
-      "2001:20::/28"
-      "2001:db8::/32"
-      "2002::/16"
-      "fc00::/7"
-      "fe80::/10"
-      "ff00::/8"
-    ];
+  aclFile = pkgs.writeText "localIPs.acl" 
+    '' 
+      0.0.0.0/8
+      10.0.0.0/8
+      100.64.0.0/10
+      127.0.0.0/8
+      169.254.0.0/16
+      172.16.0.0/12
+      192.0.0.0/24
+      192.0.2.0/24
+      192.88.99.0/24
+      192.168.0.0/16
+      198.18.0.0/15
+      198.51.100.0/24
+      203.0.113.0/24
+      224.0.0.0/4
+      240.0.0.0/4
+      255.255.255.255/32
+      ::/128
+      ::1/128
+      ::ffff:0:0/96
+      100::/64
+      64:ff9b::/96
+      2001::/32
+      2001:10::/28
+      2001:20::/28
+      2001:db8::/32
+      2002::/16
+      fc00::/7
+      fe80::/10
+      ff00::/8
+    '';
 in
 
 { options =
@@ -91,7 +91,7 @@ in
 
           forbiddenIP = mkOption {
             type = types.listOf types.str;
-            default = localIPs;
+            default = [];
             description = "IP list declared invalid as destinations";
           };
 
@@ -175,7 +175,8 @@ in
                   ${optionalString (cfg.tcp != []) "-T '${concatStringsSep " " cfg.tcp}'"} \
                   ${optionalString (cfg.udp != []) "-U '${concatStringsSep " " cfg.udp}'"} \
                   ${optionalString (cfg.disableSOCKS5) "--disable-socks5"} \
-                  -c ${configFile}
+                  -c ${configFile} \
+                  --deny-list ${aclFile}
               '';
           };
       };
