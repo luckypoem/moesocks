137edb657424148b58555fe717343b09d98f9bc2
Jinjing Wang <nfjinjing@gmail.com>
2017-02-24 19:08:31 +0800

    add nix

---
+++ moesocks.nix
@@ -0,0 +1,21 @@
+{ mkDerivation, aeson, async, attoparsec, base, binary, bytestring
+, containers, cryptohash, hslogger, HsOpenSSL, iproute, lens
+, lens-aeson, mtl, network, optparse-applicative, random, stdenv
+, stm, strict, text, time, transformers, unordered-containers
+}:
+mkDerivation {
+  pname = "moesocks";
+  version = "1.0.0.41";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson async attoparsec base binary bytestring containers cryptohash
+    hslogger HsOpenSSL iproute lens lens-aeson mtl network
+    optparse-applicative random stm strict text time transformers
+    unordered-containers
+  ];
+  homepage = "https://github.com/nfjinjing/moesocks";
+  description = "A functional firewall killer";
+  license = stdenv.lib.licenses.asl20;
+}
